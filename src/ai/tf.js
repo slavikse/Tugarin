0da0d02d42ffa9eeea160f3fe5949d819a076ce2
slavikse <gslavikse@gmail.com>
2020-01-10 21:38:16 +0300

    upd

--- src/ai/tf.js
+++ src/ai/tf.js
@@ -1,8 +1,6 @@
 import * as tf from '@tensorflow/tfjs';
 // import { movement } from '../game/control';
 
-tf.setBackend('webgl');
-
 const a = tf.tensor([1, 2, 3, 4]);
 const b = tf.tensor([10, 20, 30, 40]);
 const y = a.add(b);
