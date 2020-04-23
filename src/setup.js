8b65eae9ab655d9fcf9bcb76f1e290b2fd73192f
slavikse <gslavikse@gmail.com>
2020-04-24 01:27:25 +0300

    Отключены браузерные горячие клавиши.

--- src/setup.js
+++ src/setup.js
@@ -1,14 +1,16 @@
 /* eslint-disable import/no-extraneous-dependencies */
-import { BrowserWindow, app } from 'electron';
+import { app, BrowserWindow, globalShortcut } from 'electron';
 
 /* eslint-disable global-require */
 if (require('electron-squirrel-startup')) {
   app.quit();
 }
 
-app.on('ready', createWindow);
+app.on('ready', ready);
 
-async function createWindow() {
+async function ready() {
+  disableBrowserHotKeys();
+
   const mainWindow = new BrowserWindow({
     x: 0,
     y: 0,
@@ -27,4 +29,10 @@ async function createWindow() {
 
   /* eslint-disable no-undef */
   await mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY);
+}
+
+function disableBrowserHotKeys() {
+  globalShortcut.register('Ctrl+W', () => {});
+  globalShortcut.register('Ctrl+R', () => {});
+  globalShortcut.register('Ctrl+M', () => {});
 }
